ee783f5cd02a974748a61528bfcdba600ffb8bfd
Nikhil Chaurasiya <rdxhacnikhil@gmail.com>
2024-09-03 22:54:05 +0530

    Add files via upload

---
+++ quadratic equation.py
@@ -0,0 +1,62 @@
+# a=1
+# b=3
+# c=0
+
+
+
+# value1=(-b-(b**2-4*a*c)**0.2)/2*a
+       
+# print(value)
+
+
+    # do the quadratic computation only if 
+    # the solution is going to be real  
+
+    # descision  making Step
+# a=4
+# b=2
+# c=1
+
+# d=b**2-4*a*c
+
+# if(d>0):
+#     print("solution is real")
+
+# else:
+#     print("solution are imaginary")
+
+
+# if(d>=0):
+#     print("solution is real")
+
+# if(d<0):
+#     print("solution are imaginary") 
+
+    #   write to program of two number devide
+# a=4   
+
+# if(a%2==0):
+#     print("a is even" ) 
+# else:
+#     print("a is odd ")
+
+
+   #    write a code to divide a by b "safely"
+
+# a=4
+# b=2   
+
+# if(b==0):
+#     print("not defined")
+# else:
+#     print(a/b)    
+    
+
+       #      given two number ,print the one which is greater
+# a=4
+# b=2
+
+# if(a>b):
+#     print("a is greater than b")
+# else:
+#     print("a is not greaten than b")
